d1bea705a9d25381975f43035bd0ed8d627d5cea
zyao2 <zhengshengy@yahoo.com>
2020-09-27 22:46:18 -0600

    Update readme.m

--- readme.m
+++ readme.m
@@ -3,4 +3,6 @@ Generating synthetic data via finite difference
 first 
    mex fwd2d_mex.c subFunc.c
 then run
-   synthetic_modeling
+   Step1_modeling --- generate synthetic data
+   Step2_time2d   --- time field 
+   Step3_migration--- migration
